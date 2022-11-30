7c0eaa2ba6a0a6b64369434388609170fba131f2
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-11-30 16:28:38 +0300

    #1 implement file system functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+
 const remove = async () => {
     // Write your code here 
+    const filePath = path.resolve("src/fs/files/fileToRemove.txt");
+
+    try {
+		await fsPromises.rm(filePath);
+	} catch (err) {
+		if (err?.code === "ENOENT") {
+			throw new Error("FS operation failed");
+		}
+		throw err;
+	}
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+
 const list = async () => {
     // Write your code here 
+    const dirPath = path.resolve("src/fs/files");
+
+    try {
+		const content = await fsPromises.readdir(dirPath);
+        console.log(content);
+	} catch (err) {
+		if (err?.code === "ENOENT") {
+			throw new Error("FS operation failed");
+		}
+		throw err;
+	}
 };
 
 await list();
