ede7d11588355317730e7ad62274f6a36604866d
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-12-02 14:21:14 +0300

    #2 basics implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+import process from 'node:process';
+
 const parseArgs = () => {
     // Write your code here 
+    console.log(
+        process.argv.slice(2).reduce((acc, currentValue, index, array) => {
+            if (currentValue.startsWith("--")) {
+                return acc + currentValue.slice(2) + " is ";
+            }
+
+            if (array.length === index + 1) {
+                return acc + currentValue;
+            }
+
+            return acc + currentValue + ", ";
+        }, "")
+    )
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import process from 'node:process';
+
 const parseEnv = () => {
     // Write your code here 
+    const keys =  Object.keys(process.env).filter((key) => key.startsWith("RSS_"));
+    const rssVars = keys.map((key) => `${key}=${process.env[key]}`).join("; ");
+    console.log(rssVars);
 };
 
 parseEnv();
