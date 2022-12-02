c1a8e8806ebff3e24f6357f0e24aafa40e8a40ab
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-12-02 15:24:08 +0300

    #3 basics esm refactor

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,25 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "node:path";
+import { release, version } from "node:os";
+import { createServer as createServerHttp } from "node:http";
+import "./files/c.js";
+
+import a from "./files/a.json" assert { type: 'json' };
+import b from "./files/b.json" assert { type: 'json' };
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b;
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +42,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
