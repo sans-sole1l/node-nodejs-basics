31c5b020cbc55cb505e81e04cda4fac60e7d339d
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-12-03 16:27:06 +0300

    #4 basics implement hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'node:fs';
+import path from "node:path";
+
+const {
+    createHash
+} = await import('node:crypto');
+
 const calculateHash = async () => {
     // Write your code here 
+    const PATH = "src/hash/files/fileToCalculateHashFor.txt";
+    const filePath = path.resolve(PATH);
+
+    const hash = createHash('sha256');
+
+    const input = createReadStream(filePath);
+
+    input.on('readable', () => {
+        const data = input.read();
+
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
