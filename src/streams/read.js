ff1e05fe9e1fe03efe4ccfc4eff4f5dfba83d5b9
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-12-05 15:31:27 +0300

    #5 basics streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import process from 'node:process';
+
 const read = async () => {
     // Write your code here 
+    const stream = createReadStream('src/streams/files/fileToRead.txt', 'utf-8');
+
+    let data = '';
+
+    stream.on('data', chunk => data += chunk);
+    stream.on('end', () => process.stdout.write(data));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from 'node:fs';
+import process from 'node:process';
+
 const write = async () => {
     // Write your code here 
+    const input = process.stdin;
+    const output = createWriteStream('src/streams/files/fileToWrite.txt', 'utf-8');
+
+    let data = '';
+
+    input.on('data', chunk => {
+        data += chunk;
+        output.write(chunk);
+    });
 };
 
 await write();
