2c51064f38e611283c2e89e3fc75e87cd0c185d9
Evgeny Proskurnikov <vesper182@inbox.ru>
2022-12-06 11:23:56 +0300

    #6 basics zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { promisify } from 'node:util';
+
 const compress = async () => {
     // Write your code here 
+    const pipe = promisify(pipeline);
+
+    const gzip = createGzip();
+    const source = createReadStream('src/zip/files/fileToCompress.txt');
+    const destination = createWriteStream('src/zip/files/archive.gz');
+
+    try {
+        await pipe(source, gzip, destination);
+    } catch (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await compress();
